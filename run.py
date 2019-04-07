c51ba84bc51525a57a2adfc9c8681816481cf8f9
Jacob E <37668374+jacobeng3l@users.noreply.github.com>
2019-04-06 22:09:04 -0400

    Add files via upload

---
+++ run.py
@@ -0,0 +1,56 @@
+import configparser
+from flask import Flask, render_template, request
+import mysql.connector
+
+# Read configuration from file.
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+# Set up application server.
+app = Flask(__name__)
+
+# Create a function for fetching data from the database.
+def sql_query(sql):
+    db = mysql.connector.connect(**config['mysql.connector'])
+    cursor = db.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    cursor.close()
+    db.close()
+    return result
+
+
+def sql_execute(sql):
+    db = mysql.connector.connect(**config['mysql.connector'])
+    cursor = db.cursor()
+    cursor.execute(sql)
+    db.commit()
+    cursor.close()
+    db.close()
+
+# For this example you can select a handler function by
+# uncommenting one of the @app.route decorators.
+
+#@app.route('/')
+def basic_response():
+    return "It works!" #example
+
+#@app.route('/')
+def template_response():
+    return render_template('home.html')
+
+@app.route('/', methods=['GET', 'POST'])
+def template_response_with_data():
+    print(request.form)
+#    if "buy-book" in request.form:
+#        book_id = int(request.form["buy-book"])
+#        sql = "delete from book where id={book_id}".format(book_id=book_id)
+#        sql_execute(sql)
+    template_data = {}
+    sql = "select song_id, name from song order by name"
+    songs = sql_query(sql)
+    template_data['songs'] = songs
+    return render_template('home-w-data.html', template_data=template_data)
+
+if __name__ == '__main__':
+    app.run(**config['app'])
---
+++ setup.sql
@@ -0,0 +1,37 @@
+--
+-- Example setup file for a web database project.
+--
+
+-- create database and user, grant privileges to user
+create database datify;
+create user 'mysql_username'@'localhost' identified by 'mysql_password';
+grant all on web_database_project.* to 'mysql_username'@'localhost';
+flush privileges;
+
+-- select the database and create tables
+use datify;
+create table album(
+    album_id int not null auto_increment primary key,
+    name varchar(255) not null,
+    date_published date,
+    image varchar(255),
+    description varchar(255)
+);
+
+create table song(
+    song_id int not null auto_increment primary key,
+    name varchar(255) not null,
+    plays int DEFAULT 0,
+    explicit int DEFAULT 0,
+    duration int not null,
+    num int not null,
+    file_loc varchar(255) not null,
+    album_id int not null,
+    FOREIGN KEY (album_id) REFERENCES album(album_id)
+);
+
+-- insert data into database
+insert into album(album_id, name) values (1, 'The Click');
+insert into song(name, duration, num, file_loc, album_id) values 
+    ('Sober Up', 218, 4, '/home', 1);
+
