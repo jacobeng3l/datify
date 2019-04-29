973db57f0d02ec9326e34686558571260d941a95
jdr145 <43837641+jdr145@users.noreply.github.com>
2019-04-29 19:30:14 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 import configparser
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 import mysql.connector
 
 # Read configuration from file.
@@ -31,19 +31,34 @@ def sql_execute(sql):
 @app.route('/', methods=['GET', 'POST'])
 def template_response_with_data():
     print(request.form)
-#    if "buy-book" in request.form:
-#        book_id = int(request.form["buy-book"])
-#        sql = "delete from book where id={book_id}".format(book_id=book_id)
-#        sql_execute(sql)
-    template_data = {}
-    sql = "select song_id, explicit, song.name, song.album_id, album.name, plays, duration, file_loc from song, album where song.album_id = album.album_id order by song.name"
-    songs = sql_query(sql)
-    template_data['songs'] = songs
+    data = {}
     if "login" in request.form:
-      return render_template('homepage.html', template_data=template_data)
+        email = str(request.form["email"])
+        password = str(request.form["psw"])
+        sql = "select user_id from user where user.email={email} and user.password={password}".format(email=email, password=password)
+        user = sql_query(sql)
+        data['user'] = user
+        return render_template('homepage.html', data=data)
+    if "delete-song" in request.form:
+        delete_song_id = int(request.form["delete-song"])
+        sql = "delete from song where song_id={delete_song_id}".format(delete_song_id=delete_song_id)
+        sql_execute(sql)
     if "library" in request.form:
-      return render_template('library.html', template_data=template_data)
-    return render_template('login_page.html', template_data=template_data)
+        user_id = int(request.form["library"])
+        sql = "select song_id, explicit, song.name, song.album_id, album.name, plays, duration, file_loc from song, album where song.album_id = album.album_id order by song.name"
+        songs = sql_query(sql)
+        data['songs'] = songs
+        data['user'] = user_id
+        return render_template('library.html', data=data)
+    if "playlists" in request.form:
+        user_id = int(request.form["playlists"])
+        sql = "select song_id, explicit, song.name, song.album_id, album.name, plays, duration, file_loc from song, album where song.album_id = album.album_id order by song.name"
+        songs = sql_query(sql)
+        data['songs'] = songs
+        data['user'] = user_id
+        return render_template('playlists.html', data=data)
+    if "friends" in request.form:
+        return render_template('friends.html', data=data)
 
 if __name__ == '__main__':
     app.run(**config['app'])
