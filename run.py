8851c991bbbb17b8e599430097775685182e5c12
jdr145 <43837641+jdr145@users.noreply.github.com>
2019-04-30 19:39:41 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -124,7 +124,7 @@ def search():
     data = {}
     data['query'] = str(request.form['search'])
     # sql query to return search results
-    sql = "select song.name from song limit 20"
+    sql = "select song.song_id, song.explicit, song.name, song.album_id, album.name, song.plays, song.duration, song.file_loc, artist.name from song, album, artist where artist.artist_id=song.artist_id and song.album_id=album.album_id order by song.name"
     results = sql_query(sql)
     data['results'] = results
     return render_template('search.html', data=data)
