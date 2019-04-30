5e3d00f9485698bcc405c43608de1fffcf2ea359
adu3 <49668286+adu3@users.noreply.github.com>
2019-04-29 21:50:46 -0400

    Update data.sql

--- data.sql
+++ data.sql
@@ -1,2 +1,65 @@
+insert into user(user_id, email, name, password) values 
+  (2, 'abc2@case.edu', 'Jacob', '2');
+insert into user(user_id, email, name, password) values 
+  (2, 'abc3@case.edu', 'Jason', '3');
+insert into user(user_id, email, name, password) values 
+  (2, 'abc4@case.edu', 'Jordan', '4');
 
-
+insert into artist(artist_id, name) values
+  (4, 'Kanye West');
+  
+insert into album(album_id, name) values
+  (4, 'Yeezus');
+  
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (5, 'On Sight', 157, 1, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (6, 'Black Skinhead', 160, 2, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (7, 'I am a God', 278, 3, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (8, 'New Slaves', 265, 4, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (9, 'Hold My Liquor', 152, 5, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (10, 'I\'m In it', 177, 6, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (11, 'Blood On the Leaves', 201, 7, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (12, 'Guilt Trip', 245, 8, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (13, 'Send It Up', 157, 9, '/home', 4, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (14, 'Bound 2', 210, 10, '/home', 4, 4);
+  
+insert into album(album_id, name) values
+  (5, 'My Beautiful Dark Twisted Fantasy');
+  
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (15, 'Dark Fantasy', 157, 1, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (16, 'Gorgeous', 168, 2, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (17, 'Power', 176, 3, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (18, 'All of the Lights (Interlude)', 189, 4, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (19, 'All of the Lights', 198, 5, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (20, 'Monster', 200, 6, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (21, 'So Appalled', 185, 7, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (22, 'Devil in a New Dress', 210, 8, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (23, 'Runaway', 238, 9, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (24, 'Hell of a Life', 170, 10, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (25, 'Blame Game', 160, 11, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (26, 'Lost in the World', 267, 12, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (27, 'Who Will Survive in America', 278, 13, '/home', 5, 4);
+insert into song(song_id, name, duration, num, file_loc, album_id, artist_id) values
+  (28, 'See Me Now', 246, 14, '/home', 5, 4);
