460d30f01a2880f1984c600ba09f1778b00e4fec
Jacob E <37668374+jacobeng3l@users.noreply.github.com>
2019-04-30 18:52:33 -0400

    Update fact.sql

--- fact.sql
+++ fact.sql
@@ -1,1 +1,11 @@
-create table plays();
+create table plays(
+  user_id int not null,
+  current_song_id int not null,
+  previous_song_id int, 
+  next_song_id int, 
+  date date not null, 
+  PRIMARY KEY (user_id, current_song_id, previous_song_id, next_song_id), 
+  FOREIGN KEY (user_id) FROM user(user_id), 
+  FOREIGN KEY (current_song_id) FROM song(song_id), 
+  FOREIGN KEY (previous_song_id) FROM song(song_id), 
+  FOREIGN KEY (next_song_id) FROM ));
