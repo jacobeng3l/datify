71df2e9232b459b86409056fb7057a4f4eb70b13
jdr145 <43837641+jdr145@users.noreply.github.com>
2019-04-30 23:24:31 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -79,7 +79,10 @@ def library():
     print(request.form)
     if "items" in request.form:
         print(request.form["items"])
-        for song_id in request.form["items"]:
+        print(request.form["items"][0])
+        print(request.form["items"][0].split(','))
+        for song_id in request.form["items"][0].split(','):
+            print(song_id)
             sql = "update song set song.plays = song.plays + 1 where song.song_id={song_id}".format(song_id=song_id)
             sql_execute(sql)
     if "delete-song" in request.form:
