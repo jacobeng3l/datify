3a5af0aa563a32f7972a93b1a74e7aace5b45a5d
adu3 <49668286+adu3@users.noreply.github.com>
2019-04-30 20:16:42 -0400

    Update fact.sql

--- fact.sql
+++ fact.sql
@@ -3,7 +3,7 @@ create table plays(
   current_song_id int not null,
   previous_song_id int, 
   next_song_id int, 
-  times timestamp not null, 
+  times datetime not null, 
   PRIMARY KEY (user_id, current_song_id, times), 
   FOREIGN KEY (user_id) REFERENCES user(user_id), 
   FOREIGN KEY (current_song_id) REFERENCES song(song_id), 
