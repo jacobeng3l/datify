297c376b1953ed93bda5c3a85899bcaa69be27de
jdr145 <43837641+jdr145@users.noreply.github.com>
2019-04-30 23:13:36 -0400

    Add files via upload

--- run.py
+++ run.py
@@ -130,8 +130,9 @@ def search():
     data = {}
     if "items" in request.form:
         print(request.form["items"])
+        print(request.form["items"][0])
         for song_id in request.form["items"]:
-            print(song_id)
+            #print(song_id)
             sql = "update song set song.plays = song.plays + 1 where song.song_id={song_id}".format(song_id=song_id)
             sql_execute(sql)
     if "add-song" in request.form:
