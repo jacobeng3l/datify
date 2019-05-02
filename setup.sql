da39f3ac0d6af1c479193a6ad4fb36b4a0bbe91a
adu3 <49668286+adu3@users.noreply.github.com>
2019-05-01 20:29:34 -0400

    Update setup.sql

--- setup.sql
+++ setup.sql
@@ -114,12 +114,10 @@ create table in_playlist(
 create table plays(
   user_id int not null,
   current_song_id int not null,
-  previous_song_id int, 
   next_song_id int, 
   times datetime not null, 
   PRIMARY KEY (user_id, current_song_id, times), 
   FOREIGN KEY (user_id) REFERENCES user(user_id), 
-  FOREIGN KEY (current_song_id) REFERENCES song(song_id), 
-  FOREIGN KEY (previous_song_id) REFERENCES song(song_id), 
+  FOREIGN KEY (current_song_id) REFERENCES song(song_id),  
   FOREIGN KEY (next_song_id) REFERENCES song(song_id)
 );
