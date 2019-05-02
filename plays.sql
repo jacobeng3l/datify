3030ea36d70e48ee974f613d39807f0bba98e397
adu3 <49668286+adu3@users.noreply.github.com>
2019-05-01 21:13:57 -0400

    Update plays.sql

--- plays.sql
+++ plays.sql
@@ -2,80 +2,80 @@
 
 -- user 1 plays a lot of songs
 insert into plays(user_id, current_song_id, times) values
-  (1, 1, current_timesstamp);
+  (1, 1, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 2, current_timesstamp);
+  (1, 2, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 3, current_timesstamp);
+  (1, 3, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 4, current_timesstamp);
+  (1, 4, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 5, current_timesstamp);
+  (1, 5, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 6, current_timesstamp);
+  (1, 6, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 7, current_timesstamp);
+  (1, 7, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 8, current_timesstamp);
+  (1, 8, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 9, current_timesstamp);
+  (1, 9, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 10, current_timesstamp);
+  (1, 10, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 11, current_timesstamp);
+  (1, 11, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 12, current_timesstamp);
+  (1, 12, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 13, current_timesstamp);
+  (1, 13, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 14, current_timesstamp);
+  (1, 14, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 15, current_timesstamp);
+  (1, 15, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 16, current_timesstamp);
+  (1, 16, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 17, current_timesstamp);
+  (1, 17, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 18, current_timesstamp);
+  (1, 18, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 19, current_timesstamp);
+  (1, 19, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (1, 20, current_timesstamp);
+  (1, 20, current_timestamp);
   
 -- user 2 plays a bunch of music
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 2, current_timesstamp);
+  (2, 2, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 2, current_timesstamp);
+  (2, 2, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 5, current_timesstamp);
+  (2, 5, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 5, current_timesstamp);
+  (2, 5, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 5, current_timesstamp);
+  (2, 5, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 5, current_timesstamp);
+  (2, 5, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 8, current_timesstamp);
+  (2, 8, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 8, current_timesstamp);
+  (2, 8, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 20, current_timesstamp);
+  (2, 20, current_timestamp);
 insert into plays(user_id, current_song_id, times) values
-  (2, 8, current_timesstamp);
+  (2, 8, current_timestamp);
